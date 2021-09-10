d8ef1a7f65e5ff555b0c9665a7885372522b7a33
Jeff <jeffahall94@gmail.com>
2021-09-10 17:14:22 -0400

    Working Image Gallery with hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import GalleryBox from "./GalleryBox";
 import './App.css';
+
+// some fake data to simulate passing in actual gallery data
+const fakeData = [{
+  title: 'NATURE VS. MAN',
+  image: './testImage.svg'
+},{
+  title: 'NATURE VS. WOMAN',
+  image: './testImage.svg'
+}
+,{
+  title: 'NATURE VS. TAXES',
+  image: './testImage.svg'
+},
+{
+  title: 'NATURE VS. NATURE',
+  image: './testImage.svg'
+},{
+  title: 'BEAR VS. MAN',
+  image: './testImage.svg'
+}
+,{
+  title: 'DOLPHIN VS. MAN',
+  image: './testImage.svg'
+},
+{
+  title: 'ELEPHANT VS. MAN',
+  image: './testImage.svg'
+},{
+  title: 'NATURE',
+  image: './testImage.svg'
+}
+,{
+  title: ' VS. ',
+  image: './testImage.svg'
+},
+{
+  title: 'NATURE MAN',
+  image: './testImage.svg'
+},{
+  title: 'MAN NATURE',
+  image: './testImage.svg'
+}
+,{
+  title: 'MAN MAN. MAN',
+  image: './testImage.svg'
+},
+{
+  title: 'VS VS. VS',
+  image: './testImage.svg'
+},{
+  title: 'NATURE VS. VS',
+  image: './testImage.svg'
+}
+,{
+  title: 'VS VS. MAN',
+  image: './testImage.svg'
+},
+{
+  title: 'MAN',
+  image: './testImage.svg'
+},{
+  title: 'MAN VS. MAN',
+  image: './testImage.svg'
+}
+,{
+  title: 'MAN VS. MAN VS. MAN',
+  image: './testImage.svg'
+}]
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Taft - Multimedia Gallery
+        </h3>
       </header>
+      <GalleryBox {...fakeData}/>
     </div>
   );
 }
---
+++ src/GalleryBox.tsx
@@ -0,0 +1,30 @@
+import { FaPlay } from "react-icons/fa";
+import image from './testImage.svg'
+interface galleryItem {
+    title: string,
+    image: string
+}
+
+// simple function component to render each video/game item in the gallery
+function GalleryBox(data: Array<galleryItem>) {
+    let items: JSX.Element[] = []
+    for(let i in data){
+        items.push(
+            <div className={'galleryItem'} key={i} style={{backgroundImage: `url(${image})`,backgroundRepeat: 'no-repeat',
+                backgroundSize: '33vh 18vh'}}>
+                <div className={'itemHover'}>
+                    <h5 className={'itemTitle'}>{data[i].title}</h5>
+                    <div className={'itemLine'}></div>
+                    <FaPlay className={'playIcon'}/>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="galleryBox">
+            {items}
+        </div>
+    );
+}
+
+export default GalleryBox;
